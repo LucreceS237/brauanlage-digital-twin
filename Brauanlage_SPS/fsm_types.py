db6b6253e20e293817f266718f5fb11d8ac59181
Adrix2022 <adrixlylwayne@gmail.com>
2026-07-03 11:25:01 +0200

    Löschen von der alten Datei Brauanlage_SPS

--- Brauanlage_SPS/fsm_types.py
+++
@@ -1,6 +0,0 @@
-# fsm_types.py – gemeinsame Typ-Aliase (nicht „types“ – Kollision mit Stdlib)
-from typing import TypeAlias
-
-from states import BrewState
-
-StateTransition: TypeAlias = tuple[BrewState, BrewState]
